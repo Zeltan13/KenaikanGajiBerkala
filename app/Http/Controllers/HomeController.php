109fec63755de647fbb3bf8090790eb00476ed33
Fathan <askarfathan@gmail.com>
2024-05-14 10:56:42 +0700

    update

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,13 +4,15 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Session;
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Models\UserData;
+
 class HomeController extends Controller
 {
     public function index(){
         $userId = Session::get('userId');
         $user = User::where('id', $userId)->first();
         echo "<pre>";
-        var_dump($user->nama);
+        var_dump($user->roleId);
         echo "</pre>";
         
         return view('home', ['data' => $user]);
@@ -22,5 +24,9 @@ class HomeController extends Controller
         // echo "</pre>";
         // //$strRole = strval($roleId);
         // return view('home', ['nama'=> "Helo"]);
+    }
+    function getAllUserData(){
+        $datas = UserData::all();
+        return view('userData',['datas'=>$datas]);
     }
 }
